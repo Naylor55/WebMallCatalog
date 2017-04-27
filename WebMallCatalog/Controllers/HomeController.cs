272c88ec6ab31f999027e8a423bc2d8ccfa1b19e
aylorNNNNNNN <aylorNNNNNNN@CHENMINGLIANG>
2017-04-27 10:32:50 +0800

    tianjaajax

--- WebMallCatalog/Controllers/HomeController.cs
+++ WebMallCatalog/Controllers/HomeController.cs
@@ -51,5 +51,18 @@ namespace WebMallCatalog.Controllers
             return "后台已经获取到了前台传递过来的数据，编号：" + Id + "-------名称：" + Name;
         }
 
+        [HttpPost]
+        public JsonResult SendForJQAjaxRequest()
+        {
+            JsonResult json = new JsonResult();
+            if (Request.Form.Count > 0)
+            {
+                int id = Convert.ToInt32(Request.Form["Id"]);
+                string name = Request.Form["Name"];
+                json.Data = new { result = "后台已经接受到了前台的JQAjax请求。Id=" + id + "-----name=" + name };
+            }
+            return json;
+        }
+
     }
 }
